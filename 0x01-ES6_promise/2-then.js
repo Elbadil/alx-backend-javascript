e3c5fde7d3c57567821457e3593470fcf4f442bb
Elbadil <elbaliadil@gmail.com>
2024-01-03 14:47:58 +0100

    handling Response From API

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
     .catch(() => {
       const error = new Error();
       return error;
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
     });
 }
