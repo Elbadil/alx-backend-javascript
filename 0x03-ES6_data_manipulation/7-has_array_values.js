1f0b68cc8967773db69ba02db1e5d795a3f19f6b
Elbadil <elbaliadil@gmail.com>
2024-01-08 18:35:06 +0100

    More set data structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,12 +1,3 @@
 export default function hasValuesFromArray(set, arr) {
-  const trueOrFalse = [];
-  for (const num of arr) {
-    trueOrFalse.push(set.has(num));
-  }
-  for (const answer of trueOrFalse) {
-    if (answer === false) {
-      return false;
-    }
-  }
-  return true;
+  return arr.every((num) => set.has(num));
 }
