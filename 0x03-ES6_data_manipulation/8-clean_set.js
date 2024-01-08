29f96e664f528d044f3edbf9434710a0a95906e4
Elbadil <elbaliadil@gmail.com>
2024-01-08 20:02:05 +0100

    Clean Set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,12 @@
 export default function cleanSet(set, startString) {
   const setString = [];
 
-  if (startString.length === 0) {
+  if (
+    startString.length === 0
+    || typeof startString !== 'string'
+    || typeof set !== 'object'
+    || !(set instanceof Set)
+    || set.length === 0) {
     return '';
   }
 
