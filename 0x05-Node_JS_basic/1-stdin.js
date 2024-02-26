5d7f2b70a4f50469c6c7c1f5a70074326c213585
Elbadil <elbaliadil@gmail.com>
2024-02-26 16:28:56 +0100

    Replaces process stdout with console log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
 const process = require('process');
 const readline = require('readline');
 
-console.log('Welcome to Holberton School, what is your name?');
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question('', (input) => {
+rl.question('Welcome to Holberton School, what is your name?\n', (input) => {
   console.log(`Your name is: ${input}`);
 
   rl.close();
