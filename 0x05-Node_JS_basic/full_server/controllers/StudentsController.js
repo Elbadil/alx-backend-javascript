40dd5517b4e0d2aab055a04c657d71a9184747ca
Elbadil <elbaliadil@gmail.com>
2024-02-27 20:35:57 +0100

    Full complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,41 @@
+import readDatabase from '../utils';
+
+export default class StudentsController {
+  static async getAllStudents(request, response) {
+    try {
+      let responseMessage = 'This is the list of our students';
+      const studentData = await readDatabase(request.dbFile);
+      for (const field in studentData) {
+        if (studentData[field]) {
+          responseMessage += `\nNumber of students in ${field}: ${studentData[field].length}. List: ${studentData[field].join(', ')}`;
+        }
+      }
+      response.statusCode = 200;
+      response.send(responseMessage);
+      return response;
+    } catch (error) {
+      response.statusCode = 500;
+      response.send(error.message);
+      return response;
+    }
+  }
+
+  static async getAllStudentsByMajor(request, response) {
+    try {
+      const studentData = await readDatabase(request.dbFile);
+      const { major } = request.params;
+      if (major === 'CS' || major === 'SWE') {
+        response.statusCode = 200;
+        response.send(`List: ${studentData[major].join(', ')}`);
+        return response;
+      }
+      response.statusCode = 500;
+      response.send('Major parameter must be CS or SWE');
+      return response;
+    } catch (error) {
+      response.statusCode = 500;
+      response.send(error.message);
+      return response;
+    }
+  }
+}
