c9e5558638817c5893332956dc3ace6dd04bf14b
Elbadil <elbaliadil@gmail.com>
2024-02-28 18:43:04 +0100

    Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,8 +6,11 @@ const { expect } = require('chai');
 describe('calculateNumber Usage', () => {
   it('should call Utils.calculateNumber with correct arguments', () => {
     const calculateNumberSpy = spy(Utils, 'calculateNumber');
+    const sendPaymentSpy = spy(console, 'log')
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(sendPaymentSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
     calculateNumberSpy.restore();
+    sendPaymentSpy.restore();
   });
 });
